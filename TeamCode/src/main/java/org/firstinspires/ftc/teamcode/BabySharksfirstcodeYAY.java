d50e62445ccc0c043459943ee40440be21b30a34
team33574 <team33574@penguincodingschool.com>
2026-02-18 16:53:35 -0500

    just a test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BabySharksfirstcodeYAY.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "BabySharksFirstCodeYAY", group = "OffSeason")
+public class BabySharksfirstcodeYAY extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello","world");
+    }
+
+    @Override
+    public void loop(){
+
+    }
+}
